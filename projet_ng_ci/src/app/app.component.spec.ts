b8baca801ec9868e886dc2a0a775a18ab8821b99
hajarbouamout <hajarbouamout@gmail.com>
2024-06-14 12:35:01 +0200

    add no standlone
    re creation of project

--- projet_ng_ci/src/app/app.component.spec.ts
+++ projet_ng_ci/src/app/app.component.spec.ts
@@ -1,10 +1,16 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [
+        RouterTestingModule
+      ],
+      declarations: [
+        AppComponent
+      ],
     }).compileComponents();
   });
 
@@ -14,7 +20,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'projet_ng_ci' title`, () => {
+  it(`should have as title 'projet_ng_ci'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('projet_ng_ci');
