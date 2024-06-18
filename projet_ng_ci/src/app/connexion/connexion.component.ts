6e19c132144e7ba37397e6cbb64156582598aa46
saousanebst <bastaoui.saousane@gmail.com>
2024-06-18 17:14:53 +0200

    failed Connexion

--- projet_ng_ci/src/app/connexion/connexion.component.ts
+++ projet_ng_ci/src/app/connexion/connexion.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'connexion',
@@ -6,5 +8,30 @@ import { Component } from '@angular/core';
   styleUrl: './connexion.component.css'
 })
 export class ConnexionComponent {
+
+  loginForm!: FormGroup;
+
+  emailCtrl!: FormControl;
+  passwordCtrl!: FormControl;
+
+  constructor(private formBuilder: FormBuilder, private authService: AuthService) {
+    this.emailCtrl = this.formBuilder.control("", Validators.required);
+    this.passwordCtrl = this.formBuilder.control("", [Validators.required,Validators.minLength(12)]);
+
+    this.loginForm = this.formBuilder.group({
+      email: this.emailCtrl,
+      password: this.passwordCtrl
+    });
+
+   
+  }
+
+  connexion() {
+    this.authService.login(this.emailCtrl.value, this.passwordCtrl.value);
+  }
+
+
+
+
 
 }
